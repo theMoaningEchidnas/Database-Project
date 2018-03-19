ba1287e22157da00561593268826b5efcb11645d
Unknown <georgu.dimitrov@gmail.com>
2018-03-19 15:03:50 +0200

    Wrapper and Renderer
    Added renderer
    Added read and write wrapper

---
+++ AnimaniaConsole/AnimaniaConsole.Core/Constants/GlobalConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimaniaConsole.Core.Constants
+{
+    public static class GlobalConstants
+    {
+        public static string[] mainMenuOptions = new string[] { "LogIn", "Register" };
+
+        public static string[] userMenuOptions = new string[] {"SearchPosts", "CreatePost", "DeletPost", "ShowMyPosts"};
+    }
+}
---
+++ AnimaniaConsole/AnimaniaConsole.Core/Contracts/IWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimaniaConsole.Core.Contracts
+{
+    public interface IWriter
+    {
+        void Write(string str);
+
+        void WriteLine(string str);
+
+        void NewLine();
+    }
+}
