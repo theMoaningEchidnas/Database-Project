4e470b1342aeadb78fe8deb3d11b623fedf1f71f
cheficha <stefanzhekov95@gmail.com>
2018-03-20 14:23:35 +0200

    Created CommandParser,CommandProcessor,CommandFactory with Interfaces

---
+++ AnimaniaConsole/AnimaniaConsole.Core/Commands/CommandProcessor.cs
@@ -0,0 +1,35 @@
+﻿using AnimaniaConsole.Core.Commands.CommandContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimaniaConsole.Core.Commands
+{
+    public class CommandProcessor : ICommandProcessor
+    {
+        public ICollection<ICommand> Commands { get; set; }
+
+        public CommandProcessor()
+        {
+            this.Commands = new List<ICommand>();
+        }
+
+        public string ProcessSingleCommand(ICommand command, string commandParameters)
+        {
+            var lineParameters = commandParameters.Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = command.Execute(lineParameters);
+            var normalizedOutput = this.NormalizeOutput(result);
+            return normalizedOutput;
+        }
+
+        private string NormalizeOutput(string commandOutput)
+        {
+            var list = commandOutput.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).ToList().Where(x => !string.IsNullOrWhiteSpace(x));
+
+            return string.Join("\r\n", list);
+        }
+    }
+}
