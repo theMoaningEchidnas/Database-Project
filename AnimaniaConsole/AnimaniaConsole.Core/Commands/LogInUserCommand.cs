612d6765a92719a282561a1fd4ab22bb876b4413
Unknown <georgu.dimitrov@gmail.com>
2018-03-22 13:34:28 +0200

    LogIn
    LogIn command added

---
+++ AnimaniaConsole/AnimaniaConsole.Core/Commands/LogInUserCommand.cs
@@ -0,0 +1,40 @@
+﻿using AnimaniaConsole.Core.Commands.CommandContracts;
+using AnimaniaConsole.DTO.Models;
+using AnimaniaConsole.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimaniaConsole.Core.Commands
+{
+    public class LogInUserCommand : ICommand
+    {
+        private readonly IUserService userService;
+        private readonly ISessionService sessionService;
+        private readonly UserSessionModel session;
+
+        public LogInUserCommand(IUserService userService, ISessionService sessionService, UserSessionModel session)
+        {
+            this.userService = userService;
+            this.sessionService = sessionService;
+            this.session = session;
+        }
+        public string Execute(IList<string> parameters)
+        {
+            string username = parameters[1];
+            string password = parameters[2];
+
+            if (sessionService.ValidateUser(session))
+            {
+                throw new Exception("There is a logged in user!");
+            }
+            else
+            {
+                userService.LogInUser(username, password, session);
+            }
+            return "User logged in successfully.";
+        }
+    }
+}
--- AnimaniaConsole/AnimaniaConsole.Data/Migrations/Configuration.cs
+++ AnimaniaConsole/AnimaniaConsole.Data/Migrations/Configuration.cs
@@ -18,74 +18,21 @@ namespace AnimaniaConsole.Data.Migrations
 
         protected override void Seed(AnimaniaConsole.Data.AnimaniaConsoleContext context)
         {
+            /*string dogString = "Dog";
+            var dog = context.AnimalTypes.Where(x => x.AnimalTypeName == dogString).Single();
 
-            var animalType = new AnimalType() { AnimalTypeName = "Dog" };
-            context.AnimalTypes.AddOrUpdate(at => at.Id, animalType);
-            context.SaveChanges();
-
-            var maxAnimalTypeId = context.AnimalTypes.Max(x => x.Id);
-
-            var breedType = new BreedType()
+            using (var webClient = new System.Net.WebClient())
             {
-                BreedTypeName = "Pudel2",
-                AnimalTypeId = maxAnimalTypeId
-            };
-            context.BreedTypes.AddOrUpdate(bt => bt.Id, breedType);
-            context.SaveChanges();
-
-            var maxBreedTypeId = context.BreedTypes.Max(x => x.Id);
-
-            var location = new Location() { LocationName = "Sofia" };
-            context.Locations.AddOrUpdate(l => l.Id, location);
-            context.SaveChanges();
-
-            var maxLocationId = context.Locations.Max(x => x.Id);
-
-            var rand = new Random().Next();
-
-            ; var user = new Models.Models.User()
-            {
-                UserName = "cheficha" + rand,
-                Password = "12345Annnnnnn",
-                FirstName = "Stefan",
-                LastName = "Zhekov",
-                Email = "asdas" + rand + "@abv.bg",
-            };
-            context.Users.AddOrUpdate(u => u.Id, user);
-            context.SaveChanges();
-
-            var maxUserId = context.Users.Max(x => x.Id);
-
-            var animal = new Animal()
-            {
-                AnimalName = "Jhony",
-                Birthday = DateTime.Parse("2015-10-12"),
-                AnimalTypeId = maxAnimalTypeId,
-                BreedTypeId = maxBreedTypeId,
-                LocationId = maxLocationId,
-                UserId = maxUserId
-            };
-            context.Animals.AddOrUpdate(a => a.Id, animal);
-            context.SaveChanges();
-
-            var maxAnimalId = context.Animals.Max(x => x.Id);
-
-            var post = new Post()
-            {
-                Id = maxAnimalId,
-                Title = "A dog for sell",
-                Description = "I sell my dog for a good price pls buy fast",
-                PostDate = DateTime.Now,
-                Price = 199m,
-                Status = true,
-                UserId = maxUserId
-            };
-            context.Posts.AddOrUpdate(p => p.Id, post);
+                var json = webClient.DownloadString("https://dog.ceo/api/breeds/list/all");
+                // Now parse with JSON.Net
+            }
+            context.BreedTypes.AddOrUpdate();
             context.SaveChanges();
 
             //Method outputs the EntityValidationErrors in PackageManager
-            EntityValidationErrorsOutput.SaveChanges(context);
+            EntityValidationErrorsOutput.SaveChanges(context);*/
 
         }
+        
     }
 }
