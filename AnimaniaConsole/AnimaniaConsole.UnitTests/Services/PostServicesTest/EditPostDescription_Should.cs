2d8820c61c24c02c01ad45153874896671e7da87
Unknown <ahristoff@abv.bg>
2018-04-01 12:50:47 +0300

    some minor changes in cproj file

---
+++ AnimaniaConsole/AnimaniaConsole.UnitTests/Services/PostServicesTest/EditPostDescription_Should.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnimaniaConsole.UnitTests.Services.PostServicesTest
+{
+    [TestClass]
+    public class EditPostDescriptionShould
+    {
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+
+        }
+
+
+        [TestMethod]
+        public void Returns_InstanceOfTypeString_When_Executed()
+        {
+
+
+        }
+
+        [TestMethod]
+        public void FindTheCorrectPostForEdit_When_Executed()
+        {
+
+
+        }
+
+        [TestMethod]
+        public void EditCorrectly_theDescrition_When_Executed()
+        {
+
+
+        }
+    }
+}
